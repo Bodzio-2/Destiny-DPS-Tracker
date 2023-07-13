0af9ff4fb1a7fb3be62ab4326162a75cacbfa122
Michał Jeske <michal.jes@gmail.com>
2023-07-13 23:20:24 +0200

    some cleaning and setting up

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 import pyautogui
 import pytesseract
 
-i=0
+i = 0
 start_x = pyautogui.size()[0]/4
 start_y = pyautogui.size()[1]/5
 
@@ -12,27 +12,22 @@ size_x = (pyautogui.size()[0]/4) * 2
 size_y = (pyautogui.size()[1]/5) * 3
 
 
-
-while(True):
-
-
+while (True):
 
     image = pyautogui.screenshot(region=(start_x, start_y, size_x, size_y))
 
     image = cv2.cvtColor(np.array(image),
                          cv2.COLOR_RGB2BGR)
 
-    
-    
     blurred = cv2.GaussianBlur(image, (5, 5), 1)
-    mask = cv2.inRange(blurred,(0,180,200),(20,240,255))
+    mask = cv2.inRange(blurred, (0, 180, 200), (20, 240, 255))
     res = 255 - mask
 
+    # edged = cv2.Canny(blurred, 50, 200, 255)
+    # gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 
-    #edged = cv2.Canny(blurred, 50, 200, 255)
-    #gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-
-    ret, thresh1 = cv2.threshold(res, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    ret, thresh1 = cv2.threshold(
+        res, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
@@ -40,7 +35,7 @@ while(True):
 
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
                                            cv2.CHAIN_APPROX_NONE)
-    
+
     im2 = res.copy()
 
     file = open("parsedText.txt", "w+")
@@ -50,13 +45,14 @@ while(True):
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
 
-        rect = cv2.rectangle(im2, (x,y), (x + w, y+h), (0, 255, 0), 2)
+        rect = cv2.rectangle(im2, (x, y), (x + w, y+h), (0, 255, 0), 2)
 
         cropped = im2[y:y + h, x: x+w]
 
         file = open("parsedText.txt", "a")
 
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' #Here replace with Tesseract-OCR path
+        # Here replace with Tesseract-OCR path
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
         text = pytesseract.image_to_string(cropped)
         print(text)
@@ -66,9 +62,8 @@ while(True):
         file.write("\n")
         file.close()
 
-    #cv2.imwrite("./gray/image" + str(i) + ".png", res)
-    #cv2.imwrite("./dilation/im" + str(i) + ".png", dilation)
-    #cv2.imwrite("./contours/im" + str(i) + ".png", blurred)
-    i+=1
+    # cv2.imwrite("./gray/image" + str(i) + ".png", res)
+    # cv2.imwrite("./dilation/im" + str(i) + ".png", dilation)
+    # cv2.imwrite("./contours/im" + str(i) + ".png", blurred)
+    i += 1
     time.sleep(1)
-
