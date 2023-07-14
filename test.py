30a584971755d6f9b0760040c73adf97eac9616f
Michał Jeske <michal.jes@gmail.com>
2023-07-14 02:26:00 +0200

    michal_tests

--- test.py
+++ test.py
@@ -3,31 +3,54 @@ import numpy as np
 import time
 import pyautogui
 import pytesseract
+import os
+
+print("----------------------------")
+
+
+def writeAnnouncement(message):
+    print(message)
+    print("----------------------------" + "\n")
+
+
+def removeFiles(dir):
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+
+
+writeAnnouncement("Cleaning folders")
+
+removeFiles('images/contours/')
+removeFiles('images/dilation/')
+removeFiles('images/gray/')
+removeFiles('images/processed/')
+
+writeAnnouncement("Folders cleaned, Starting program")
 
 i = 0
-start_x = pyautogui.size()[0]/4
-start_y = pyautogui.size()[1]/5
+found = False
+start_x = pyautogui.size()[0]/6
+start_y = pyautogui.size()[1]/8
 
-size_x = (pyautogui.size()[0]/4) * 2
-size_y = (pyautogui.size()[1]/5) * 3
+size_x = (pyautogui.size()[0]/4) * 3
+size_y = (pyautogui.size()[1]/5) * 4
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 file = open("parsedText.txt", "w+")
 
-while (i != 10):
+while (i != 200):
 
     image = pyautogui.screenshot(region=(start_x, start_y, size_x, size_y))
 
     image = cv2.cvtColor(np.array(image),
                          cv2.COLOR_RGB2BGR)
+    
+    # blurred = cv2.GaussianBlur(image, (5, 5), 1)
 
-    blurred = cv2.GaussianBlur(image, (5, 5), 1)
-    mask = cv2.inRange(blurred, (0, 180, 200), (20, 240, 255))
+    mask = cv2.inRange(image, (0, 140, 160), (50, 240, 255))
+  
     res = 255 - mask
-
-    # edged = cv2.Canny(blurred, 50, 200, 255)
-    # gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 
     ret, thresh1 = cv2.threshold(
         res, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
@@ -49,19 +72,25 @@ while (i != 10):
         cropped = im2[y:y + h, x: x+w]
 
         text = pytesseract.image_to_string(cropped)
+        text=text.split()
+        text=text[0]
         print(text)
         cv2.imwrite("images/processed/" + str(i) +
                     "_" + str(j) + ".png", cropped)
 
-        file.write(text)
-        file.write("\n")
+        if(text!=''):
+            file.write(text + "\n")
         j += 1
+        found = True
 
-    cv2.imwrite("images/gray/" + str(i) + ".png", res)
-    cv2.imwrite("images/dilation/" + str(i) + ".png", dilation)
-    cv2.imwrite("images/contours/" + str(i) + ".png", blurred)
+    if (found):
+        cv2.imwrite("images/gray/" + str(i) + ".png", res)
+        cv2.imwrite("images/dilation/" + str(i) + ".png", dilation)
+        found = False
+
+    cv2.imwrite("images/contours/" + str(i) + ".png", image)
 
     i += 1
 
-    time.sleep(1)
+    time.sleep(0.2)
 file.close()
